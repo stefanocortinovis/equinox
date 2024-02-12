89e1b51c70467926f6b8bcbd84823ef8d7988527
Stefano Cortinovis <stefanocortinovis17@gmail.com>
2024-02-12 21:34:35 +0000

    Refactor _nexafter.py for TPU

--- equinox/internal/_nextafter.py
+++ equinox/internal/_nextafter.py
@@ -6,13 +6,12 @@ from jaxtyping import Array
 @jax.custom_jvp
 def nextafter(x: Array) -> Array:
     """Returns the floating point number after `x`."""
-    y = jnp.nextafter(x, jnp.inf)
     # Flush denormal to normal.
     # Our use for these is to handle jumps in the vector field. Typically that means
     # there will be an "if x > cond" condition somewhere. However JAX uses DAZ
     # (denormals-are-zero), which will cause this check to fail near zero:
     # `jnp.nextafter(0, jnp.inf) > 0` gives `False`.
-    return jnp.where(x == 0, jnp.finfo(x.dtype).tiny, y)  # pyright: ignore
+    return x + jnp.finfo(x).smallest_normal
 
 
 @nextafter.defjvp
@@ -25,8 +24,7 @@ def nextafter_jvp(primals, tangents):
 @jax.custom_jvp
 def prevbefore(x: Array) -> Array:
     """Returns the floating point number before `x`."""
-    y = jnp.nextafter(x, -jnp.inf)
-    return jnp.where(x == 0, -jnp.finfo(x.dtype).tiny, y)  # pyright: ignore
+    return x - jnp.finfo(x).smallest_normal
 
 
 @prevbefore.defjvp
